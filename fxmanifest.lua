4d670acfad8f41a4ad9d9ed8a8bbee2fe4c10eb4
fdev23 <91542369+fdev23@users.noreply.github.com>
2021-09-28 17:15:58 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts{
+    '@es_extended/locale.lua',
+    'config.lua',
+    'blip.lua'
+}
+
+
+server_scripts{
+    '@es_extended/locale.lua',
+    'config.lua',
+    'server.lua'
+}
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+}
+
